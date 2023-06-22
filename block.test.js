8352402b07a7876229eb0e1d3bff8932877750bf
Aakarshit <aakarshit.agarwal@gmail.com>
2023-06-22 07:02:31 +0530

    Configuring Babel

---
+++ block.test.js
@@ -0,0 +1,29 @@
+import Block from './block';
+import { GENESIS_DATA } from './config';
+
+describe('Block', () => {
+    const timestamp = '';
+    const lastHash = '';
+    const hash = '';
+    const data = [];
+    const block = new Block({ timestamp, lastHash, hash, data });
+
+    it('has a timestamp, lastHash, hash, and data', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a Block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+    });
+});
