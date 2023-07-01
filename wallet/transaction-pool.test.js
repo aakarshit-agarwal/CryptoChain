d03d3a3de1d04ce346efc6ad0771bed6240eb2e4
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-02 00:20:12 +0530

    Adding validTransaction functionality for transaction pool

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -35,4 +35,44 @@ describe('TransactionPool', () => {
             ).toBe(transaction);
         });
     });
+
+    describe('validTransaction()', () => {
+        let validTransactions, errorMock;
+
+        beforeEach(() => {
+            validTransactions = [];
+            errorMock = jest.fn();
+            global.console.error = errorMock;
+
+            for (let i = 0; i < 10; ++i) {
+                transaction = new Transaction({
+                    senderWallet,
+                    recipient: 'any-recipient',
+                    amount: 40,
+                });
+
+                if (i % 3 === 0) {
+                    transaction.input.amount = 9999999;
+                } else if (i % 3 === 1) {
+                    transaction.input.signature = new Wallet().sign(
+                        'some-data'
+                    );
+                } else {
+                    validTransactions.push(transaction);
+                }
+                transactionPool.setTransaction(transaction);
+            }
+        });
+
+        it('returns valid transactions', () => {
+            expect(transactionPool.validTransactions()).toEqual(
+                validTransactions
+            );
+        });
+
+        it('logs errors for the invalid transactions', () => {
+            transactionPool.validTransactions();
+            expect(errorMock).toHaveBeenCalled();
+        });
+    });
 });
