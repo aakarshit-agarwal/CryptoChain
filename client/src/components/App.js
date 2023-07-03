a7e9631303f096055d6c63cc5c9bce46215b7f78
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-03 10:49:11 +0530

    Adding app styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Blocks from './Blocks';
+import logo from '../assets/logo.png';
 
 class App extends Component {
     state = { walletInfo: {} };
@@ -12,13 +13,17 @@ class App extends Component {
     render() {
         const { address, balance } = this.state.walletInfo;
         return (
-            <>
+            <div className='App'>
+                <img className='logo' src={logo}></img>
+                <br></br>
                 <div>Welcome to blockchain...</div>
-                <div>Address: {address}</div>
-                <div>Balance: {balance}</div>
+                <div className='WalletInfo'>
+                    <div>Address: {address}</div>
+                    <div>Balance: {balance}</div>
+                </div>
                 <br></br>
                 <Blocks></Blocks>
-            </>
+            </div>
         );
     }
 }
