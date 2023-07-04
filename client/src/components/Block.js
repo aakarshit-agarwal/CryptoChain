fe8c5997c740cb2125708ba8b9bb99042217a6a1
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-04 17:50:45 +0530

    Adding Transaction component

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
     state = { displayTransaction: false };
@@ -19,7 +20,14 @@ class Block extends Component {
         if (this.state.displayTransaction) {
             return (
                 <div>
-                    {stringifiedData}
+                    {data.map((transaction) => (
+                        <div key={transaction.id}>
+                            <hr></hr>
+                            <Transaction
+                                transaction={transaction}
+                            ></Transaction>
+                        </div>
+                    ))}
                     <br></br>
                     <Button
                         bsStyle='danger'
