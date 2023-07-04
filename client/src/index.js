ec8e2a5447957c4832c280fca879289b45a24a4f
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-04 21:16:51 +0530

    Adding page to create a transaction

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 import Blocks from './components/Blocks.js';
+import ConductTransaction from './components/ConductTransaction.js';
 
 render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />}></Route>
             <Route path='/blocks' element={<Blocks />}></Route>
+            <Route
+                path='/conduct-transaction'
+                element={<ConductTransaction />}
+            ></Route>
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
