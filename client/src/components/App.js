7f658ac3a416ea5a936e5869ff380f38061f6c4a
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-04 20:11:45 +0530

    Adding seaparate page for blocks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -17,12 +17,14 @@ class App extends Component {
                 <img className='logo' src={logo}></img>
                 <br></br>
                 <div>Welcome to blockchain...</div>
+                <br></br>
+                <div>
+                    <Link to='/blocks'>Blocks</Link>
+                </div>
                 <div className='WalletInfo'>
                     <div>Address: {address}</div>
                     <div>Balance: {balance}</div>
                 </div>
-                <br></br>
-                <Blocks></Blocks>
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
+import Blocks from './components/Blocks.js';
 
-render(<App></App>, document.getElementById('root'));
+render(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />}></Route>
+            <Route path='/blocks' element={<Blocks />}></Route>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
